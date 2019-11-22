e259f28584969c85764b4c6b5a467f9dd581eb1c
gozdeylmz95 <gozdeylmz95@gmail.com>
2019-11-22 19:40:24 +0300

    todo-app-homework

---
+++ src/TodoList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Todo from './Todo';
+
+
+class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div className="todo-list">
+                <h3>{this.props.title}
+                -
+                {this.props.todos.length}  {/* kaç adet olduğunu yazmak için */}
+                </h3>
+                {
+                    this.props.todos.map((todo) => {
+                        return <Todo
+                            {...todo}
+                            key={todo.id}
+                            onTodoRemove={this.props.onTodoRemove}
+                            onCheckedToggle={this.props.onCheckedToggle}
+                        />
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default TodoList;
